6c5fe670b9a6642b51b98cf414da1639322f5f55
Chinmay-G <chinmay.the1@gmail.com>
2025-07-24 18:54:36 +0530

    Integrate database backend for trending movies

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -3,6 +3,7 @@ import SearchBar from "@/components/SearchBar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
 import { fetchMovies } from "@/services/api";
+import { updateSearchCount } from "@/services/appwrite";
 import useFetch from "@/services/useFetch";
 
 import { useEffect, useState } from "react";
@@ -24,6 +25,10 @@ const Search = () => {
     const timeoutId = setTimeout(async () => {
       if (searchQuery.trim()) {
         await loadMovies();
+        console.log(movies?.length);
+
+        if (movies?.length > 0 && movies[0])
+          await updateSearchCount(searchQuery, movies[0]);
       } else {
         reset();
       }
